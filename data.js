be2b3aedc5c2533293451545e1f662487825430f
Thomas <thomaschou9177@gmail.com>
2025-07-20 15:46:14 +0800

    Version 12

--- data.js
+++ data.js
@@ -2,4 +2,4 @@ fetch(`https://api-dattebayo.vercel.app/cards`)
 .then(response=>response.json())
 .then(value=>console.log(value))
 .catch(error=>console.error(error));
-console.log(`version 11`);
+console.log(`version 12`);
